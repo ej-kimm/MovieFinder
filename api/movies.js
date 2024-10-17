a82e297533d2a319f7d6812e4d70603e941c3f3a
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-10-17 14:57:55 +0900

    👽️ Implement searchMovie API function for movie title search

--- api/movies.js
+++ api/movies.js
@@ -1,10 +1,10 @@
 const API_KEY = '22fbac1adbd9708d5158d657e4a4dbb4'
-const MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie'
+const MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3'
 const IMAGE_API_BASE_URL = 'https://image.tmdb.org/t/p/original'
 
 export async function getPopularMovies() {
   return fetch(
-    `${MOVIE_API_BASE_URL}/popular?api_key=${API_KEY}&language=ko-KR&page=1`
+    `${MOVIE_API_BASE_URL}/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`
   )
     .then((response) => response.json())
     .then((data) => data.results)
@@ -13,4 +13,13 @@ export async function getPopularMovies() {
 
 export function getMovieImage(url) {
   return `${IMAGE_API_BASE_URL}/${url}`
+}
+
+export async function getSearchMovie(title) {
+  return fetch(
+    `${MOVIE_API_BASE_URL}/search/movie?query=${title}&api_key=${API_KEY}&language=ko-KR&page=1`
+  )
+    .then((response) => response.json())
+    .then((data) => data.results)
+    .catch((err) => console.error(err))
 }
