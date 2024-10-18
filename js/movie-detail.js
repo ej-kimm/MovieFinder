d29673bb2bc88e9e1b3559d70acf3ee938485c68
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-10-18 18:46:05 +0900

    💄 Implement movie-detail component

--- js/movie-detail.js
+++ js/movie-detail.js
@@ -1,3 +1,5 @@
+import { getMovieImage } from '../api/movies.js'
+
 const movieDetail = document.querySelector('.movie-detail')
 const poster = document.querySelector('.poster')
 const movieDetailInfo = document.querySelector('.movie-detail-info')
@@ -7,14 +9,29 @@ const overview = movieDetailInfo.querySelector('.overview dd')
 const releaseDate = movieDetailInfo.querySelector('.release-date dd')
 const rating = movieDetailInfo.querySelector('.rating dd')
 const closeBtn = document.querySelector('.btn-close')
+const overlay = document.querySelector('.overlay')
 
 // modal
 export function updateMovieDetail(movie) {
   if (movie) {
+    poster.style.backgroundImage = `url(${getMovieImage(movie.poster_path)})`
+    title.textContent = movie.title
+    genre.innerHTML = movie.genres
+      .map(
+        (genre) => `<button class="btn-primary btn-32">${genre.name}</button>`
+      )
+      .join(' ')
+    overview.textContent = movie.overview
+    releaseDate.textContent = movie.release_date
+    rating.textContent = '⭐'.repeat(Math.floor(movie.vote_average))
+    overlay.style.backgroundImage = `url(${getMovieImage(movie.backdrop_path)})`
+
     movieDetail.classList.toggle('is-active')
+    overlay.classList.toggle('is-active')
   }
 }
 
 closeBtn.addEventListener('click', function () {
   movieDetail.classList.toggle('is-active')
+  overlay.classList.toggle('is-active')
 })
