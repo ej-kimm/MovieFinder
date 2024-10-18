d8b3a8aa80429979a1395f82616e9bb31b574fb1
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-10-18 17:27:48 +0900

    💄 Implement movie-detail component

--- js/movie-detail.js
+++ js/movie-detail.js
@@ -1,4 +1,20 @@
+const movieDetail = document.querySelector('.movie-detail')
+const poster = document.querySelector('.poster')
+const movieDetailInfo = document.querySelector('.movie-detail-info')
+const title = movieDetailInfo.querySelector('.title dd')
+const genre = movieDetailInfo.querySelector('.genre dd')
+const overview = movieDetailInfo.querySelector('.overview dd')
+const releaseDate = movieDetailInfo.querySelector('.release-date dd')
+const rating = movieDetailInfo.querySelector('.rating dd')
+const closeBtn = document.querySelector('.btn-close')
+
 // modal
 export function updateMovieDetail(movie) {
-  console.log(movie)
+  if (movie) {
+    movieDetail.classList.toggle('is-active')
+  }
 }
+
+closeBtn.addEventListener('click', function () {
+  movieDetail.classList.toggle('is-active')
+})
