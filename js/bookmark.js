560ec756dbea778d776af1eab0b8bfa83a97f814
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-10-22 01:01:29 +0900

    💄 Implement bookmark component

---
+++ js/bookmark.js
@@ -0,0 +1,22 @@
+export function isMovieBookmarked(movieId) {
+  const bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || []
+  return bookmarks.find((bookmark) => bookmark.id === movieId)
+}
+
+export function saveToBookmark(movie) {
+  const bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || []
+
+  if (!isMovieBookmarked(movie.id)) {
+    localStorage.setItem('bookmarks', JSON.stringify([...bookmarks, movie]))
+    alert(`✅[${movie.title}] 북마크에 저장 완료!`)
+  }
+}
+
+export function removeToBookmark(movie) {
+  const bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || []
+  const filteredBookmarks = bookmarks.filter(
+    (bookmark) => bookmark.id !== movie.id
+  )
+  localStorage.setItem('bookmarks', JSON.stringify(filteredBookmarks))
+  alert(`❌[${movie.title}] 북마크 해제 완료!`)
+}
--- js/movie-detail.js
+++ js/movie-detail.js
@@ -1,4 +1,9 @@
 import { getMovieImage } from '../api/movies.js'
+import {
+  isMovieBookmarked,
+  removeToBookmark,
+  saveToBookmark,
+} from './bookmark.js'
 
 const movieDetail = document.querySelector('.movie-detail')
 const poster = document.querySelector('.poster')
@@ -8,12 +13,17 @@ const genre = movieDetailInfo.querySelector('.genre dd')
 const overview = movieDetailInfo.querySelector('.overview dd')
 const releaseDate = movieDetailInfo.querySelector('.release-date dd')
 const rating = movieDetailInfo.querySelector('.rating dd')
+const bookmarkBtn = movieDetailInfo.querySelector('.btn-bookmark')
+const removeBookmarkBtn = movieDetailInfo.querySelector('.btn-remove-bookmark')
 const closeBtn = document.querySelector('.btn-close')
 const overlay = document.querySelector('.overlay')
+
+let currentMovie = null
 
 // modal
 export function updateMovieDetail(movie) {
   if (movie) {
+    currentMovie = movie
     poster.style.backgroundImage = `url(${getMovieImage(movie.poster_path)})`
     title.textContent = movie.title
     genre.innerHTML = movie.genres
@@ -30,9 +40,32 @@ export function updateMovieDetail(movie) {
 
     movieDetail.classList.toggle('is-active')
     overlay.classList.toggle('is-active')
+    updateBookmarkButtons(movie.id)
   }
 }
 
+function updateBookmarkButtons(movieId) {
+  if (isMovieBookmarked(movieId)) {
+    bookmarkBtn.style.display = 'none'
+    removeBookmarkBtn.style.display = 'block'
+  } else {
+    bookmarkBtn.style.display = 'block'
+    removeBookmarkBtn.style.display = 'none'
+  }
+}
+
+// 북마크 버튼 클릭 이벤트
+bookmarkBtn.addEventListener('click', function () {
+  saveToBookmark(currentMovie)
+  updateBookmarkButtons(currentMovie.id)
+})
+
+removeBookmarkBtn.addEventListener('click', function () {
+  removeToBookmark(currentMovie)
+  updateBookmarkButtons(currentMovie.id)
+})
+
+// 모달 창 닫기 버튼 클릭 이벤트
 closeBtn.addEventListener('click', function () {
   movieDetail.classList.toggle('is-active')
   overlay.classList.toggle('is-active')
