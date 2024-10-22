323a07e527423eb7be18716302d5978c85dd511c
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2024-10-22 14:28:23 +0900

    ♻️ Refactor input event to use debouncing for improved efficiency

--- js/search-bar.js
+++ js/search-bar.js
@@ -3,12 +3,23 @@ import { updateMovieCards } from './movie-list.js'
 const searchInput = document.querySelector('.search-input')
 const searchButton = document.querySelector('.search-button')
 
+const debounce = (callback, delay) => {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => callback.apply(this, args), delay)
+  }
+}
+
 searchButton.addEventListener('click', function () {
   const query = searchInput.value.toLowerCase()
   query && updateMovieCards(query)
 })
 
-searchInput.addEventListener('input', function (e) {
-  const query = searchInput.value.toLowerCase()
-  query ? updateMovieCards(query) : updateMovieCards()
-})
+searchInput.addEventListener(
+  'input',
+  debounce(function () {
+    const query = searchInput.value.toLowerCase()
+    query ? updateMovieCards(query) : updateMovieCards()
+  }, 300)
+)
